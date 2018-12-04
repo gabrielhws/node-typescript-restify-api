ecee7a400277287e18a49920839a58349430ccbf
gabriel <gabrielhws@gmail.com>
2018-12-04 12:47:08 -0200

    Commit env 1.2

---
+++ README.md
@@ -0,0 +1,20 @@
+## SAMPLE Restful API WITH RESTIFY
+
+## Install
+
+0.  Run `npm install -g typescript` to install TS ->
+    [TypeScript](https://www.typescriptlang.org)
+1.  Clone this repo
+    `https://github.com/gabrielhws/node-typescript-restify-api.git`
+1.  Run `npm install` to install dependencies.<br />
+1.  Run `npm run tsc` to compile and create /dist/\*.js .<br />
+1.  Run `npm start` to see the app at `http://localhost:3000`
+
+## Lint
+
+Powered by Prettier
+
+## Deploy
+
+1.  Run `npm run build` to compile TS to ES6
+2.  Execute with pm2 `pm2 start pm2.json`
--- middlewares/db.ts
+++ middlewares/db.ts
@@ -46,8 +46,22 @@ export class DB {
         DB.setUri()
           .then(function(url) {
             (<any>mongoose).Promise = global.Promise;
+            const options = {
+              autoIndex: false, // Don't build indexes
+              reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+              reconnectInterval: 500, // Reconnect every 500ms
+              poolSize: 10, // Maintain up to 10 socket connections
+              // If not connected, return errors immediately rather than waiting for reconnect
+              bufferMaxEntries: 0,
+              connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+              socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+              family: 4, // Use IPv4, skip trying IPv6
+            };
             mongoose
-              .connect(url)
+              .connect(
+                url,
+                options
+              )
               .then(() => {
                 const db = mongoose.connection;
                 db.on('error', function(err) {
